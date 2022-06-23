0be8caef502641495a9831e8c31e6cd641844f0f
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-06-23 10:48:19 -0500

    Mostrar enemigo 2 y ajustar colision de enemigo vs Personaje

--- javascript/script.js
+++ javascript/script.js
@@ -45,7 +45,7 @@ personajeElvis.src = "../images/enemigo-1.png";
 
 //----------VILLANO 2 "PERRO 2"
 let personajeBella = new Image()
-personajeBella.scr = "../images/perrito.png";
+personajeBella.src = "../images/perrito.png";
 
 //----------CHORRITO DE AGUA
 const aguaImagen = new Image()
@@ -96,25 +96,26 @@ function actualizarEscenario(){
     crearEnemigos()
 
 
-//----------------ARREGLO - ENEMIGOS
+//----------------ARREGLO - ENEMIGOS/POSICIÓN-----------------------------------
     enemigos.forEach((enemigo, index) => { 
         enemigo.x -= 2
         enemigo.dibujarse()
-        if(enemigo.x === moy.x + 50 && enemigo.y === moy.y) {
-            moy.recibirDano(20)
+        if(enemigo.x === moy.x + 70 && (enemigo.y >= moy.y && enemigo.y <= moy.y+90
+             || enemigo.y+90 >= moy.y && enemigo.y+90 <= moy.y+90)) {
+            moy.recibirDano(0.5)
             enemigos.splice(index, 1)
         }
         
     })
 
 
-//---------------ARREGLO - DISPAROS DE AGUA    
-    agua.forEach((agua, indexAgua) => {
-        agua.x += 2
-        agua.dibujarse()
+//---------------ARREGLO - DISPAROS DE AGUA-----------------    
+    agua.forEach((balaAgua, indexAgua) => {
+        balaAgua.x += 2
+        balaAgua.dibujarse()
 
         enemigos.forEach((enemigo, indexEnemigo) => {
-            if(enemigo.x === agua.x || enemigo.x === agua.x + 2 || enemigo.x === agua.y - 2){
+            if(enemigo.x === balaAgua.x || enemigo.x === balaAgua.x + 2 || enemigo.x === balaAgua.y - 2){
                 enemigos.splice(indexEnemigo, 1)
                 agua.splice(indexAgua, 1)
             }
@@ -164,26 +165,22 @@ document.addEventListener("keydown", (event) => {
             moy.moverAtras();console.log("Mover a la izquierda")
             moy.img = personajeMoyIzq
         }
-       
         break;
     case "ArrowRight":
         if(moy.x + 100 < canvas.width){
             moy.moverAlFrente(); console.log("Mover a la derecha")
             moy.img = personajeMoy
-        }
-        
+        }        
         break;
 
     case "ArrowDown":
         if(moy.y + 200 < canvas.height){
             moy.moverAbajo();  console.log("Abajo")
             moy.img = personajeMoy
-        }
-        
+        }       
         break;
     
     case "ArrowUp":
-       
         if(moy.y - 85 > 0 ){
             moy.moverArriba();  console.log("Arriba")
             moy.img = personajeMoy
@@ -192,9 +189,9 @@ document.addEventListener("keydown", (event) => {
 
     case " ":
         if (agua.length < 30){
-        const nuevaAgua = moy.disparar(moy.x+70, moy.y-10, aguaImagen)
-        agua.push(nuevaAgua)
-    }
+            const nuevaAgua = moy.disparar(moy.x+70, moy.y-10, aguaImagen)
+            agua.push(nuevaAgua)
+        }
         break;
   
     }
